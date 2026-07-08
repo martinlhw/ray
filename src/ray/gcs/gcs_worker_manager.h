7a8988dadd30bb28a06f923a7b5b54bb01205862
hyungwoo.lee <hyungwoo.lee@anyscale.com>
2026-07-08 16:15:10 +0000

    adding dead worker id q and methods
    Signed-off-by: hyungwoo.lee <hyungwoo.lee@anyscale.com>

--- src/ray/gcs/gcs_worker_manager.h
+++ src/ray/gcs/gcs_worker_manager.h
@@ -14,6 +14,7 @@
 
 #pragma once
 
+#include <deque>
 #include <vector>
 
 #include "ray/gcs/gcs_kv_manager.h"
@@ -67,10 +68,14 @@ class GcsWorkerManager : public rpc::WorkerInfoGcsServiceHandler {
   void SetUsageStatsClient(UsageStatsClient *usage_stats_client) {
     usage_stats_client_ = usage_stats_client;
   }
+  /// This is only used to restore the queue after GCS restarts
+  void RestoreDeadWorkerIdsQueue();
 
  private:
   void GetWorkerInfo(const WorkerID &worker_id,
                      Postable<void(std::optional<rpc::WorkerTableData>)> callback) const;
+
+  void UpdateDeadWorkerIdsQueue(const WorkerID &worker_id);
 
   gcs::GcsTableStorage &gcs_table_storage_;
   instrumented_io_context &io_context_;
@@ -94,6 +99,10 @@ class GcsWorkerManager : public rpc::WorkerInfoGcsServiceHandler {
       "Number of worker failures that are not intentional. For example, worker failures "
       "due to system related errors.",
       /*unit=*/""};
+
+  /// Queue of dead worker ids in death order (oldest at front); bounds retention in the
+  /// worker table. Only accessed on io_context_.
+  std::deque<WorkerID> dead_worker_ids_queue_;
 };
 
 }  // namespace gcs
